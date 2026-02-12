3b7c744d8fefe4ddda2e85d237650c0257f9d3d5
ZeMove228 <azzzzamov@gmail.com>
2026-02-12 20:43:01 +0500

    register endpoints and JWT login routes

--- backend/bandboost-backend/users/views.py
+++ backend/bandboost-backend/users/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .serializers import MeSerializer
+from .serializers import MeSerializer, RegisterSerializer
+from rest_framework import generics, permissions
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response(MeSerializer(request.user).data)
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
